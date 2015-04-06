ff8982a3527cf42f3206f945f2e7c689c7ef80b6
samejack <sj@toright.com>
2015-04-06 09:50:15 +0800

    fix global class name

---
+++ Universal-plus.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Universal JavaScript package PLUS!
+ * JavaScript, NodeJS Module and RequireJS Module Supported
+ */
+(function (name, definition) {
+  if (typeof(module) !== 'undefined' && module.exports) {
+    module.exports = definition;
+  } else if (typeof(define) === 'function') {
+    define(definition);
+  } else {
+    this[name] = definition();
+  }
+}('Universal', function (initServerHostname, initLogger, request) {
+  var privateVar = 'I_AM_PRIVATE_VAR';
+
+  var privateFn = function () {
+    console.log('I am private function.');
+  };
+
+  // constructor
+  var constructor = function () {
+    console.log(privateVar);
+    privateFn();
+  }();
+
+  var publicFn = function () {
+    console.log('I am public function.');
+  };
+
+  return {
+    /**
+     * Public defined
+     */
+    publicVar: 'I_AM_PUBLIC_VAR',
+
+    /**
+     * Public function
+     */
+    publicFn: publicFn
+  };
+}));
