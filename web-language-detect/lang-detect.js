cfd5d3e62ffd151e5338de59e32059f727bd1345
SJ Chou <sj@toright.com>
2016-04-12 23:22:22 +0800

    add blog web language sample code

--- web-language-detect/lang-detect.js
+++ web-language-detect/lang-detect.js
@@ -1,3 +1,50 @@
-/**
- * Created by sj on 2016/4/12.
- */
+try {
+  var switchPage = function (language) {
+    switch (language) {
+      case 'zh-cn':
+        console.log('/zh-CN' + window.location.pathname);
+        window.location.href = '/zh-CN' + window.location.pathname;
+        return true;
+        break;
+
+      case 'zh':
+      case 'zh-tw':
+        console.log('/zh-TW' + window.location.pathname);
+        window.location.href = '/zh-TW' + window.location.pathname;
+        return true;
+        break;
+
+      case 'en':
+      case 'en-us':
+        console.log('/en-US' + window.location.pathname);
+        window.location.href = '/en-US' + window.location.pathname;
+        return true;
+        break;
+
+      default:
+    }
+    return false;
+  };
+
+  // detect window.navigator.languages
+  var found = false;
+  if (typeof(window.navigator.languages) === 'object') {
+    for (var index in window.navigator.languages) {
+      console.log(window.navigator.languages[index].toLowerCase());
+      found = switchPage(window.navigator.languages[index].toLowerCase());
+      if (found) break;
+    }
+  }
+
+  if (!found) {
+    var lang = window.navigator.userLanguage || window.navigator.language;
+    var relang = lang.toLowerCase();
+    found = switchPage(relang);
+  }
+
+  if (!found) {
+    window.location.href = '/en-US' + window.location.pathname;
+  }
+} catch (e) {
+  window.location.href = '/en-US' + window.location.pathname;
+}
